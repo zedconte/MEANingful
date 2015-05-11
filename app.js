2686a320c2c030b5d6cce450933cf41af72fe709
alexismoscow <alexey.turlapov@GDLA-LT-140811B.ad.unosquare.com>
2015-05-11 14:57:32 -0500

    Fixing error

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
-
 /**
  * Module dependencies.
  */
+(function() {
+	var express = require('express'), 
+		router = require('./routes/router'), 
+		http = require('http'), 
+		path = require('path'), 
+		favicon = require('serve-favicon'), 
+		bodyParser = require('body-parser'),
+		methodOverride = require('method-override'),
+		errorHandler = require('errorhandler'),
+		morgan = require('morgan');
 
-var express = require('express')
-	,router = require('./routes/router')
-   , http = require('http')
-  , path = require('path');
+	var app = express();
 
-var app = express();
+	// all environments
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', path.join(__dirname, 'public/views'));
+	app.set('view engine', 'jade');
+	// app.use(favicon());
+	app.use(morgan('dev'));
+	app.use(bodyParser());
+	app.use(methodOverride());
+	
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views',path.join(__dirname,'public/views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
+	app.use(express.static(path.join(__dirname, 'public')));
+	app.use('/bower_components', express.static(path.join(__dirname,
+			'/bower_components')));
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/bower_components', express.static(path.join(__dirname,'/bower_components')));
+	// development only
+	if ('development' === app.get('env')) {
+		app.use(errorHandler());
+	}
 
-// development only
-if ('development' === app.get('env')) {
-  app.use(express.errorHandler());
-}
+	router(app);
 
-router(app);
+	http.createServer(app).listen(app.get('port'), function() {
+		console.log('Express server listening on port ' + app.get('port'));
+	});
 
-
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+})();
